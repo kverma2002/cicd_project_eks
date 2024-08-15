577822beb2ce7a2b61030e51492c765ff493a4a2
Krit Verma <kverma@usc.edu>
2024-08-15 16:32:09 -0400

    feat: UI Finalised Converter

--- frontend/src/components/ui/StagedFiles.jsx
+++ frontend/src/components/ui/StagedFiles.jsx
@@ -1,38 +1,70 @@
 import React from 'react';
 import { Combobox } from "@/components/ui/combobox";
+import photoImage from "@/assets/image.png";
+import videoImage from "@/assets/video.png";
 
 function StagedFiles({ files, onFormatChange, onDelete}) {
-  console.log("files", files);
+  // console.log("files", files);
+
+  function truncateFilename(filename) {
+    const maxLength = 30;
+    const extensionIndex = filename.lastIndexOf(".");
+    const extension = extensionIndex > 0 ? filename.substring(extensionIndex) : '';
+    const baseName = extensionIndex > 0 ? filename.substring(0, extensionIndex) : filename;
+
+    if (filename.length > maxLength) {
+      // Calculate the maximum length the base name can be (accounting for the ellipsis and extension length)
+      const maxBaseLength = maxLength - 3 - extension.length;
+      return `${baseName.substring(0, maxBaseLength)}...${extension}`;
+    }
+
+    return filename;
+  }
+
+  function getFilePlaceholder(fileType) {
+    // Check if the fileType includes 'video' or 'image' and return the corresponding placeholder
+    if (fileType.startsWith('video')) {
+      return videoImage;
+    } else if (fileType.startsWith('image')) {
+      return photoImage;
+    }
+    // Default placeholder if needed
+    return 'https://via.placeholder.com/50';
+  }
+
   return (
     <div className="flex flex-col items-center w-full mt-4 md:mt-8 lg:mt-12 mx-4">
       <ul className="w-full max-w-2xl lg:max-w-5xl space-y-2">
         {files.map((fileItem, index) => (
           <li
             key={index}
-            className="flex items-center justify-between p-2 bg-gray-100 dark:bg-black rounded-md"
+            className="relative flex items-center"
           >
-            <div className="flex items-center space-x-4">
+          <div className='relative flex flex-wrap items-center justify-between w-full px-4 py-4 space-y-2 border cursor-pointer lg:py-0 rounded-xl h-fit lg:h-20 lg:px-10 lg:flex-nowrap'>
+            <div className="flex items-center gap-4">
               <img
-                src="https://via.placeholder.com/50"
+                src={getFilePlaceholder(fileItem.file.type)}
                 alt="File Icon"
                 className="w-10 h-10 rounded"
               >
               </img>
-              <div className="text-gray-700 dark:text-gray-300">
-                <span className='font-medium'>{fileItem.file.name}</span> 
+              <div className="flex items-center gap-1 w-96">
+                <span className='font-medium'>{truncateFilename(fileItem.file.name)}</span> 
                 <span className="text-sm ml-2 text-gray-500">
                   ({(fileItem.file.size / 1024 / 1024).toFixed(2)} MB)
                 </span>
               </div>
             </div>
-            <div className="flex items-center space-x-2">
-              <span className='font-sm text-gray-500'>
+            <div className="flex items-center gap-4 text-muted-foreground text-md">
+              <span>
                 Convert to:
               </span>
               <Combobox 
                 currentFormat={fileItem.file.type.split("/")[0]}
                 onFormatChange={(newFormat) => onFormatChange(index, newFormat)}
               />
+            </div>
+            <span className='flex items-center justify-center w-10 h-10 text-2xl rounded-full cursor-pointer hover:bg-muted text-foreground'>
               <button
                 onClick={() => onDelete(index)}
                 className="text-red-500 hover:text-red-700 focus:outline-none"
@@ -40,7 +72,8 @@ function StagedFiles({ files, onFormatChange, onDelete}) {
               >
                 &times;
               </button>
-            </div>
+            </span>
+          </div>
           </li>
         ))}
       </ul>
--- frontend/src/pages/converter.jsx
+++ frontend/src/pages/converter.jsx
@@ -9,6 +9,7 @@ function Converter() {
 
     const [files, setFiles] = useState([]);
     const [downloadUrl, setDownloadUrl] = useState(null);
+    const [downloading, setDownloading] = useState(false);
 
     const handleFilesUpload = (uploadedFiles) => {
         // Initialize each file with a default format selection
@@ -39,7 +40,7 @@ function Converter() {
           formData.append(`files`, item.file);
           formData.append(`formats[]`, item.format);
         });
-        
+        setDownloading(true);
         const blob = await uploadFiles(formData);
         const url = window.URL.createObjectURL(blob);
         setDownloadUrl(url);
@@ -48,6 +49,7 @@ function Converter() {
         console.error('Error converting files:', error);
         // Handle the error, such as showing an error message
       }
+      setDownloading(false);
     };
 
     const handleDeleteFile = (indexToDelete) => {
@@ -57,34 +59,50 @@ function Converter() {
 
     return (
       <div className="bg-white dark:bg-black flex-col justify-center">
-          <h1 className="text-center text-2xl md:text-3xl lg:text-4xl md:m-6 lg:m-10 font-bold text-gray-900 dark:text-gray-100 px-10 sm:px-6">File Converter</h1>
-          <p className="text-center text-sm md:text-base lg:text-lg text-gray-900 dark:text-gray-200 max-w-2xl mx-auto">
-            Upload files to convert them to a different format. Supported formats are: <br />
-            <span className="text-blue-500 dark:text-blue-300">
-              Image: png, jpeg, jpg, gif, webp, tiff, tif, heif, heic, avif, svg, raw <br />
-            </span>
-            <span className="text-green-500 dark:text-green-300">
-              {' '}Video: mp4, mov, wmv, avi, mkv, flv, webm, mpeg, mpg
-            </span>
-          </p>
+          <div className="space-y-6">
+            <h1 className="text-center text-2xl md:text-3xl lg:text-4xl md:m-6 lg:m-10 font-bold text-gray-900 dark:text-gray-100 px-10 sm:px-6">File Converter</h1>
+            <p className="text-center text-sm md:text-base lg:text-lg text-gray-900 dark:text-gray-200 max-w-2xl mx-auto">
+              Upload files to convert them to a different format. Supported formats are: <br />
+              <span className="text-blue-500 dark:text-blue-300">
+                Image: png, jpeg, jpg, gif, webp, tiff, tif, heif, heic, avif, svg, raw <br />
+              </span>
+              <span className="text-green-500 dark:text-green-300">
+                {' '}Video: mp4, mov, wmv, avi, mkv, flv, webm, mpeg, mpg
+              </span>
+            </p>
+          </div>
           {files.length === 0 && (
             <>
               <FileUpload onFilesUpload={handleFilesUpload} />
             </>
           )}
           {files.length > 0 && (
-            <div className="flex flex-col justify-center items-center w-full ">
+            <div className="flex flex-col justify-center items-center w-full">
               <StagedFiles 
                 files={files} 
                 onFormatChange={handleFormatChange} 
                 onDelete={handleDeleteFile} />
-              <Button onClick={clearFiles} className="mt-4"> Clear Files </Button>
-              <Button onClick={handleConvertFiles} className="mt-4"> Convert Files </Button>
-                      {downloadUrl && (
-                <a href={downloadUrl} download="converted_files.zip" className="btn-download">
-                  Download Converted Files
-                </a>
-              )}
+              <div className="flex items-center justify-center w-full">
+                {!downloadUrl ? (
+                  <Button onClick={handleConvertFiles} disabled={files.length === 0 || downloading} className="m-4">
+                    {downloading ? <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+                      : "Convert Files"}
+                  </Button>
+                ) : (
+                  <a href={downloadUrl} download="converted_files.zip" className="m-4">
+                    <Button>
+                      Download
+                    </Button>
+                  </a>
+                )}
+                
+                <Button onClick={clearFiles} className="m-4"> Clear Files </Button>
+                {/* {downloadUrl && (
+                  <a href={downloadUrl} download="converted_files.zip" className="btn-download">
+                    Download Converted Files
+                  </a>
+                )} */}
+              </div>
             </div>
           )}
           
