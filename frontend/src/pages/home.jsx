eaa17946de8cb445924b09292c7663a05ae8429f
Krit Verma <kverma@usc.edu>
2025-02-26 16:52:15 -0500

    Test CI/CD with EKS Deployment

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div className="flex-col justify-center items-center">
         <h1 className="text-center text-2xl md:text-3xl lg:text-4xl md:m-6 lg:m-10 font-bold text-gray-900 dark:text-gray-100 px-10 sm:px-6" >Your Own File Converter</h1>
-        <p className="text-center text-sm md:text-base lg:text-lg text-gray-900 dark:text-gray-200 mx-auto"> Host your own file converter locally or on the cloud to make sure your files are secure.</p>
+        <p className="text-center text-sm md:text-base lg:text-lg text-gray-900 dark:text-gray-200 mx-auto"> Host your own file converter locally or on the cloud to make sure your files are secure!</p>
     </div>
   )
 }
